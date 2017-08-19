94817fdd57139f456e060e63217b3360bb187825
Don Zarraonandia <ddzarrao@gmail.com>
2017-08-18 19:58:32 -0700

    Lots of work
    All the work done when chelsea was in newport

---
+++ MemeMe/ImageStruct.swift
@@ -0,0 +1,17 @@
+//
+//  ImageStruct.swift
+//  MemeMe
+//
+//  Created by Donald Zarraonandia on 8/18/17.
+//  Copyright © 2017 Donald Zarraonandia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText:String
+    let bottomText:String
+    let originalImage:UIImage
+    let memedImage:UIImage
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var upperTextField: UITextField!
     @IBOutlet weak var lowerTextField: UITextField!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var navigationBar: UINavigationBar!
     
     
     let memeTextAttributes:[String:Any] = [
@@ -23,20 +25,61 @@ UINavigationControllerDelegate {
         NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
         NSStrokeWidthAttributeName: 5.0]
     
+    func generateMemedImage() -> UIImage {
+        
+       
+        toolBar.isHidden = true
+        navigationBar.isHidden = true
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        
+        toolBar.isHidden = false
+        navigationBar.isHidden = false
+        
+        return memedImage
+    }
+    
+    
+    
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+
+    }
     
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        super.viewWillAppear(true)
+        subscribeToKeyboardNotifications()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         upperTextField.defaultTextAttributes = memeTextAttributes
         upperTextField.textAlignment = .center
         lowerTextField.defaultTextAttributes = memeTextAttributes
         lowerTextField.textAlignment = .center
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
     
 
     @IBAction func pickAnImage(_ sender: Any) {
@@ -57,6 +100,40 @@ UINavigationControllerDelegate {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = image
             picker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    
+    
+    func keyboardWillShow(_ notification:Notification) {
+        
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(_ notification:Notification){
+        
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+   
+    @IBAction func shareMeme(_ sender: Any) {
+        func save() {
+            // Create the meme
+            let meme = Meme(topText: upperTextField.text!, bottomText: lowerTextField.text!, originalImage: imageView.image!, memedImage: generateMemedImage())
         }
     }
     
